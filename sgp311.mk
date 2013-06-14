2e6bd55ac907f5f5b06bef5f77561818c05a9b55
nycbjr <nycbjr@gmail.com>
2013-06-14 15:36:29 -0400

    rebasing

---
+++ sgp311.mk
@@ -0,0 +1,24 @@
+# Copyright (C) 2013 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit the common definitions
+$(call inherit-product, device/sony/sgp3xx-common/sgp3xx-common.mk)
+
+DEVICE_PACKAGE_OVERLAYS += device/sony/sgp311/overlay
+
+# Device specific part for two-stage boot
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/recovery/bootrec-device:recovery/bootrec-device
+
+$(call inherit-product-if-exists, vendor/sony/sgp311/sgp311-vendor.mk)
